f0f0a793c8b82a11ae659e94e68a1b81fb5f9237
samiafilal <sami.afilal@gmail.com>
2024-07-23 14:32:59 +0200

    Added a floating expand icon on the preview screen

--- src/Screen.svelte
+++ src/Screen.svelte
@@ -1,54 +1,15 @@
 <script lang="ts">
     import { listen } from '@tauri-apps/api/event';
-
-    let question: string = "Quel est le plus grand désert du monde ?";
-    let answers: string[] = [
-        "Le désert du Sahara",
-        "Le désert de Gobi",
-        "Le désert d'Antarctique",
-        "Le désert d'Arabie"
-    ];
-
-    // Declare the type of the payload
-    type QuestionWithAnswers = {
-        question: string;
-        answers: string[];
-    };
-
-    listen('updateScreen', (event) => {
-        const payload = event.payload as QuestionWithAnswers;
-        question = payload.question;
-        answers = payload.answers;
-    });
+    import QuestionWithAnswers from './lib/QuestionWithAnswers.svelte';
+    import CreateTeam from './lib/CreateTeam.svelte';
 </script>
 
-<main class="container"> 
-    <div class="questionWithAnswers">
-        <div class="question">{question || " "}</div>
-        <div class="flexRow">
-            <div class="answer">{"A - "+answers[0]}</div>
-            <div class="answer">{"B -"+answers[1]}</div>
-        </div>
-        <div class="flexRow">
-            <div class="answer">{"C -"+answers[2]}</div>
-            <div class="answer">{"D  -"+(answers[3])}</div>
-        </div>
-    </div>
+<main class="container">
+    <CreateTeam/>
+    <i class="fas fa-expand expand-icon"></i> <!-- Font Awesome expand icon -->
 </main>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
-
-    /* Définition des couleurs */
-    :root {
-        --primary-color: #1a252f; /* Couleur principale (bleu très foncé) */
-        --secondary-color: #34495e; /* Couleur secondaire (bleu foncé) */
-        --accent-color: #27ae60; /* Couleur d'accentuation (vert foncé) */
-        --hover-color: #2980b9; /* Couleur au survol (bleu foncé) */
-        --background-gradient-start: #422c50; /* Dégradé de fond début */
-        --background-gradient-end: #4a6073; /* Dégradé de fond fin */
-    }
-
     .container {
         display: flex;
         justify-content: center;
@@ -59,69 +20,14 @@
         margin: auto;
         padding: 0;
         font-family: 'Montserrat', sans-serif; /* Choix de police */
+        position: relative; /* Make the container the reference for absolute positioning */
     }
-
-    .questionWithAnswers {
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 60%;
-        height: auto;
-        padding: 2vh;
-        background-color: #1a252f;
-        box-shadow: 0 0 2vh rgba(0, 0, 0, 0.1);
-        animation: fadeIn 1s ease-in-out; /* Animation d'apparition */
-        overflow: hidden; /* Ajouté pour masquer le pseudo-élément débordant */
-    }
-
-    
-
-    .question {
-        white-space: pre-wrap;
-        color: #ffffff; /* Texte blanc */
-        width: 100%;
-        padding: 1.5vh;
-        border: 0.1vh solid var(--primary-color);
-        border-radius: 1vw;
-        font-size: 1.45vw;
-        text-align: center;
-        background-color: var(--secondary-color);
-        transition: background-color 0.3s ease; /* Transition pour l'effet dynamique */
-    }
-
-   
-
-    .flexRow {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        width: 100%;
-    }
-
-    .answer {
-        color: #ffffff; /* Texte blanc */
-        width: 48%;
-        padding: 1.5vh;
-        border: 0.1vh solid var(--primary-color);
-        border-radius: 1vw;
-        text-align: center;
-        background-color: var(--secondary-color);
-        transition: background-color 0.3s ease, transform 0.3s ease; /* Transition pour l'effet dynamique */
-        font-size: 1.45vw;
-    }
-
-
-    /* Animation d'apparition */
-    @keyframes fadeIn {
-        from {
-            opacity: 0;
-            transform: translateY(-1vh);
-        }
-        to {
-            opacity: 1;
-            transform: translateY(0);
-        }
+    .expand-icon {
+        position: absolute;
+        top: 10px; /* Adjust as needed */
+        right: 10px; /* Adjust as needed */
+        font-size: 24px; /* Adjust as needed */
+        color: #000; /* Adjust as needed */
+        cursor: pointer;
     }
 </style>
