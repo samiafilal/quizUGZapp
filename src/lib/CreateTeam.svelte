81ae05b237cb74d6a0e269d86fb500fbb077d0cc
samiafilal <sami.afilal@gmail.com>
2024-07-24 08:50:42 +0200

    Missing files

---
+++ src/lib/CreateTeam.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import {QRCodeImage} from "svelte-qrcode-image"
+</script>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 100%;
+    width : 100%;
+    background: linear-gradient(90deg, #422c50, #4a6073);
+  }
+</style>
+
+<div class="container">
+    <h1>Créez votre équipe  </h1>
+    <QRCodeImage text="https://quizugz.fr/createTeam" width={300}  />
+</div>
---
+++ src/lib/Welcome.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width : 100%;
+    background: linear-gradient(90deg, #422c50, #4a6073);
+  }
+</style>
+
+<div class="container">
+    <h1>Bienvenue !</h1>
+</div>
